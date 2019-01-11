cb529d6133c8d50b918c3efda660f50a5f7d11e3
Niels Mook <n.l.c.mook@student.tudelft.nl>
2019-01-11 16:16:08 +0100

    Remove subgit

---
+++ example-spi-master/Debug/source/HL_sys_pmm.d
@@ -0,0 +1,29 @@
+# FIXED
+
+source/HL_sys_pmm.obj: ../source/HL_sys_pmm.c
+source/HL_sys_pmm.obj: C:/Users/nlcmo/workspace_v8/project1/example-spi-master/include/HL_sys_pmm.h
+source/HL_sys_pmm.obj: C:/Users/nlcmo/workspace_v8/project1/example-spi-master/include/HL_reg_pmm.h
+source/HL_sys_pmm.obj: C:/Users/nlcmo/workspace_v8/project1/example-spi-master/include/HL_sys_common.h
+source/HL_sys_pmm.obj: C:/Users/nlcmo/workspace_v8/project1/example-spi-master/include/HL_hal_stdtypes.h
+source/HL_sys_pmm.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h
+source/HL_sys_pmm.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h
+source/HL_sys_pmm.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h
+source/HL_sys_pmm.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h
+source/HL_sys_pmm.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h
+source/HL_sys_pmm.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h
+source/HL_sys_pmm.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h
+source/HL_sys_pmm.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h
+
+../source/HL_sys_pmm.c: 
+C:/Users/nlcmo/workspace_v8/project1/example-spi-master/include/HL_sys_pmm.h: 
+C:/Users/nlcmo/workspace_v8/project1/example-spi-master/include/HL_reg_pmm.h: 
+C:/Users/nlcmo/workspace_v8/project1/example-spi-master/include/HL_sys_common.h: 
+C:/Users/nlcmo/workspace_v8/project1/example-spi-master/include/HL_hal_stdtypes.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h: 
